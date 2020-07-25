083bbf668d4951c3821c7e92cf30a34e09694b90
OlatunjiOniyide <olatunji.oniyide@tech11.com>
2020-07-25 23:10:19 +0100

    fix unterminated double quote String

--- src/example/initialize.rs
+++ src/example/initialize.rs
@@ -7,7 +7,7 @@ extern crate serde_json;
 use reqwest::Error;
 
 pub async fn main() -> Result<(), Error> {
-    let paystack = Paystack::new("sk_test_xxxxxxxxzzzzz.to_string());
+    let paystack = Paystack::new("sk_test_xxxxxxxxzzzzz".to_string());
     let transaction = paystack.initialize("a@a.com".to_string(), "5000".to_string()).await;
     println!("{:#?}", transaction);
     Ok(())
